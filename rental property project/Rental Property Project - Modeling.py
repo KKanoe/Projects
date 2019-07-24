1bdc64a02e83734762a32f2a600b90d569354cb9
KKanoe <KKanoe5@yahoo.com>
2019-07-24 16:22:37 -0600

    Add files via upload

--- rental property project/Rental Property Project - Modeling.py
+++ rental property project/Rental Property Project - Modeling.py
@@ -10,9 +10,6 @@ from datetime import date
 #Load rental data
 rental_data = pd.read_csv('C:/datascience/springboard/projects/Rental Property ROI/data/Rental Data Update.csv', parse_dates=['date'], infer_datetime_format=True) 
 
-rental_data = rental_data.set_index('RegionName')
-rental_view = rental_data.loc[['Riverdale']].reset_index()
-
 #Calculate rental income
 VACANT_MOS = 1 #Assumed months of vacancy (1 month is standard) 
 rental_data.loc[:, 'gross_rent'] = rental_data['Neighborhood_Zri_SingleFamilyResidenceRental'] * ((12 - VACANT_MOS) / 12)
@@ -92,18 +89,39 @@ rental_data.loc[:, 'dep_recap'] = np.where(rental_data['gain_loss'] > 0, DEP_REC
 rental_data.loc[:, 'cap_gain'] = rental_data['gain_loss'] + rental_data['dep_recap']
 rental_data.loc[:, 'capgl_tax'] = rental_data['cap_gain'] * CG_TAX_RATE #Can only deduct loss of up to $25K/year if income below $100K
 rental_data.loc[:, 'after_tax_proceeds'] = rental_data['gross_sale_proceeds'] - rental_data['mort_balance'] - rental_data['capgl_tax'] 
+rental_data.loc[:, 'cfs_plus_sale'] = rental_data['after_tax_proceeds'] + rental_data['net_cf'] 
 
-#Calculate IRR 
-def irr_calc(df):
+rental_data = rental_data.reset_index()
+rental_data = rental_data.set_index('RegionName')
+rental_view = rental_data.loc[['Southeast Como']].reset_index()
+
+region_list = rental_data['RegionID'].unique().tolist() 
+#Calculate IRR. Input dataframe and month of expected sale of property 
+region_dfs = []
+
+def irr_calc(df, sale_month):
+    #Iterate through each region 
     for i in region_list:
-        df = rental_data[rental_data['RegionID'] == i]
-        initial_inv = df['Neighborhood_Zhvi_SingleFamilyResidence'].iloc[0] * -DOWN_PMT
-        cfs = df['net_cf']
-        cfs_sale = df['net_cf'] + df['after_tax_proceeds']
-        df['irr_cf_only'] = np.irr([initial_inv, cfs])
-        df['irr_cf_sale'] = np.irr([initial_inv, cfs_sale])
+        df_new = df[df['RegionID'] == i].reset_index()
+        if sale_month > len(df_new):
+            print('Not enough data!')
+            break
+        else:        
+            initial_inv = df_new['Neighborhood_Zhvi_SingleFamilyResidence'].iloc[0] * -DOWN_PMT
+            sale_cf = df_new['cfs_plus_sale'].iloc[sale_month-1]
+            cf_list = [initial_inv]
+        #Create running list of cfs to input into np.irr function
+        for index, row in df_new.iterrows():
+            if index < sale_month-1:
+                cf_list.append(row['net_cf'])
+                df_new.loc[index, 'cf_irr'] = np.irr(cf_list)
+            else: 
+                cf_list.append(row['cfs_plus_sale'])
+                df_new.loc[index, 'cf_irr'] = np.irr(cf_list)   
+        region_dfs.append(df_new)
 
-
+rental_data_new = pd.concat(region_dfs)
+          
 #As described in observation one above. Let's begin by comparing each state to see if any signifcant differences occur amongst
 #price-to-rent ratios. Additionally, removing all properties with price-to-rent ratios above 14 (lower is better).
 rental_subset = rental_data[(rental_data['Neighborhood_PriceToRentRatio_AllHomes'] <= 1000) & 
@@ -119,7 +137,7 @@ feature_combo = []
 def feature_comp(df, feature_col):     
     
     #Get rollup statistics for desired feature (mean, std, samp_var, count
-    df_agg = df.groupby(feature_col)['Neighborhood_PriceToRentRatio_AllHomes'].agg(['mean', 'std','count']).reset_index()
+    df_agg = df.groupby(feature_col)['net_cf'].agg(['mean', 'std','count']).reset_index()
     df_agg['samp_var'] = df_agg['std'] **2 / df_agg['count']
     
     #Iterate through all combinations of given feature calculating differences for mean, std 
@@ -136,7 +154,7 @@ def feature_comp(df, feature_col):
             else:
                 continue
 #Input desired df and feature_col
-feature_comp(rental_subset, 'State')
+feature_comp(rental_subset, 'RegionID')
                 
 #Form new df for each combination, then calculate z_score, p_value, and determine is Ho can be rejected.
 feature_df = pd.DataFrame({'feature_combo':feature_combo, 'mean_diff':mean_diff, 'std_diff':std_diff})  
@@ -150,60 +168,3 @@ reject_null_df = feature_df[feature_df['reject_null'] == 'Yes']
 print('Percent significant of total combinations: %.3f' % (len(reject_null_df) / len(mean_diff)))
 print(reject_null_df[['feature_combo','z_score','p_value','reject_null']].sort_values('z_score'))
 
-
-
-
-
-
-
-
-mkt_val = []
-rent_yr = []
-taxes = []
-op_exp = [] #Includes insurance
-debt_service = []
-net_cash_flow = []
-sales_price = []
-sale_exp = [] #Includes cost of sale expenses, debt balance repayment, and capital gain taxes 
-#Capital gain taxes = Purchase Price - Depreciation +- gain/loss - depreciation recapture = cap_gain
-
-def net_cash_flow(df, region_col, purchase_date_eom): #Must be end of month corresponding to data ('YYYY-MM-DD')
-    #Loop through each region selected
-    uni_reg_list = df[region_col].sort_values().unique().tolist()
-    #Purchase Price/Market Value
-    purchase_date_eom = pd.to_datetime(purchase_date_eom)
-    df_sub = df_sub[df_sub['date'] >= purchase_date_eom]
-    if not (df_sub['date'].iloc[0] == purchase_date_eom and (0.15 > est_op_exp > 1)): #We want the purchase date to be the beginning of calc for all regions to make them comparable
-        print('Date not available or check your operating expense input')
-        break
-    else:
-
-        gross_rent = df.groupby(region_col)['Neighborhood_Zri_SingleFamilyResidenceRental'].apply(lambda x: round(x.iloc[0] * (x/12),0))
-        for i in uni_reg_list:
-            df_sub_new = df_sub[df_sub[region_col] == i]
-            purchase_price = df_sub_new['Neighborhood_Zhvi_SingleFamilyResidence'].iloc[0]
-            gross_rent = round((i - (i/12)), 0)  #Subtracts 1/12 of rent for pro-rated vacancy (assumes one month)
-            ebitda = gross_rent * (1-est_op_exp) #Need to follow some rule for this (Repairs and CapEx)
-            taxes = 
-            
-
-rental_view_yr = rental_view_yr.groupby([pd.Grouper(freq='A'), 'RegionName']).agg({'Neighborhood_Zri_SingleFamilyResidenceRental':
-    lambda x: (x.mean() * (12 - vacant_mos/12)) * x.count(),'Neighborhood_Zhvi_SingleFamilyResidence':'mean'})            
-            
-rental_view_yr = rental_view_yr.rename(columns={'Neighborhood_Zri_SingleFamilyResidenceRental':'gross_yr_rent','Neighborhood_Zhvi_SingleFamilyResidence':'est_mv'})
-    
-
-    #Calculate income
-    for i in df['Neighborhood_Zri_SingleFamilyResidenceRental']:
-            gross_rent = round((i - (i/12)), 0)  #Subtracts 1/12 of rent for pro-rated vacancy (assumes one month)
-            if est_op_exp > 1:
-                print('Check Your Estimated Operating Expense Percentage!')
-                break
-            else:
-            gross_rent = gross_rent * (1-est_op_exp) #Estimate % of monthly rent should be allocated to expenses #Is there better way? 
-            gross_rent = 
-            #Calculate Expenses
-        for j in df['']              
-    
-    for i in range(years):
-        
